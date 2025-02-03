9725c916839778da5ffa5d4defe681637ffe68e6
person3113 <person3113@gmail.com>
2025-02-03 16:33:46 +0900

    3-10 추천 기능 추가하기

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -2,18 +2,12 @@ package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -40,4 +34,7 @@ public class Question {
   private SiteUser author;
 
   private LocalDateTime modifyDate;
+
+  @ManyToMany
+  Set<SiteUser> voter;
 }
